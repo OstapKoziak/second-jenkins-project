f749e39288caaf91ba2818dbf191b203a98957b5
Ostap <ostapoz1997@gmail.com>
2024-11-04 21:08:11 -0500

    update workers

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -25,7 +25,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
